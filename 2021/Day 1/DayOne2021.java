24d267722b97f07ba8d42abd1ded3fda775559a3
zwsimone <zwsimone@gmail.com>
2022-12-01 22:24:55 +0200

    Use custom Java helper class. Add part 2 of 2021 day 1 challenge.

--- 2021/Day 1/DayOne2021.java
+++ 2021/Day 1/DayOne2021.java
@@ -1,31 +1,51 @@
 import java.io.File;
 import java.util.Scanner;
+import JavaHelpers.FileHandler;
 
 public class DayOne2021 {
     public static void main(String[] args) {
-        try {
-            File input = new File("./2021/Day 1/input.txt");
-            Scanner inputReader = new Scanner(input);
-            int index = 0;
-            int currentDepth = 0;
-            int increasedCounter = 0;
-            while (inputReader.hasNextLine()) {
-                if (index == 0) {
-                    currentDepth = Integer.parseInt(inputReader.nextLine());
-                } else {
-                    int nextDepth = Integer.parseInt(inputReader.nextLine());
-                    if (nextDepth > currentDepth) {
-                        increasedCounter++;
-                    }
-                    currentDepth = nextDepth;
-                }
-                index++;
+        int[] depthArr = FileHandler.getIntArray("./2021/Day 1/input.txt");
+        
+        // Part 1
+        int part1 = getNumberOfMeasurements(depthArr);
+        System.out.println("Number of measurements that are larger than the previous measurement: " + part1);
+
+        // Part 2
+        int part2 = getNumberOfSums(depthArr);
+        System.out.println("Number of sums that are larger than the previous sum: " + part2);
+    }
+
+    public static int getNumberOfMeasurements(int[] inputs) {
+        int currentDepth = inputs[0];
+        int increasedCounter = 0;
+        for (int i = 1; i < inputs.length; i++) {
+            int nextDepth = inputs[i];
+            if (nextDepth > currentDepth) {
+                increasedCounter++;
             }
-            inputReader.close();
-            System.out.println("Number of measurements that are larger than the previous measurement: " + increasedCounter);
-        } catch (Exception e) {
-            System.out.println("File not found.");
-            e.printStackTrace();
+            currentDepth = nextDepth;
         }
+        return increasedCounter;
+    }
+
+    public static int getNumberOfSums(int[] inputs) {
+        int currentDepth = sumOfThree(inputs, 0);
+        int increasedCounter = 0;
+        for (int i = 1; i < inputs.length; i++) {
+            if (i+2 < inputs.length) {
+                int nextDepth = sumOfThree(inputs, i);
+                if (nextDepth > currentDepth) {
+                    increasedCounter++;
+                }
+                currentDepth = nextDepth;
+            } else {
+                break;
+            }
+        }
+        return increasedCounter;
+    }
+
+    public static int sumOfThree(int[] arr, int index) {
+        return arr[index] + arr[index+1] + arr[index+2];
     }
 }
