81366992618403e2a964d717ea1bfd0b2d2937b8
zwsimone <zwsimone@gmail.com>
2022-12-01 09:35:24 +0200

    Add part 2 to Day1 file.

--- 2022/Day 1/Day1.java
+++ 2022/Day 1/Day1.java
@@ -1,12 +1,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- * Part1
- */
 public class Day1 {
 
     public static void main(String[] args) {
@@ -29,15 +27,29 @@ public class Day1 {
             }
             // Close file scanner
             inputReader.close();
+
+            // Part 1
             // Find the elf with the most calories
-            int maxCalorie = findMaxCaloriesFromElves(elves);
-            System.out.println("The Elf carrying the most calories has " + maxCalorie + " calories.");
+            // int maxCalorie = findMaxCaloriesFromElves(elves);
+            // System.out.println("The Elf carrying the most calories has " + maxCalorie + " calories.");
+
+            // Part 2
+            // Find total calories of top 3 elves
+            int maxCalories = findTotalCaloriesOfTopThreeElves(elves);
+            System.out.println("The total calories carried by the top three Elves has " + maxCalories + " calories.");
         } catch (FileNotFoundException e) {
             System.out.println("File not found.");
             e.printStackTrace();
         }
     }
 
+    private static int findTotalCaloriesOfTopThreeElves(List<Integer> elves) {
+        Collections.sort(elves, Collections.reverseOrder());
+        int totalCaloriesOfTopThreeElves = elves.get(0) + elves.get(1) + elves.get(2);
+        return totalCaloriesOfTopThreeElves;
+    }
+
+    // Part 1
     private static int findMaxCaloriesFromElves(List<Integer> elves) {
         int max = 0;
         for (int elf : elves) {
@@ -47,4 +59,6 @@ public class Day1 {
         }
         return max;
     }
+
+    // Part 2
 }
