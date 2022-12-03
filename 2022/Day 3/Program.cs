e76ab9e5c29a7c3ca5be00f3dd05ab45532fdc77
zwsimone <zwsimone@gmail.com>
2022-12-03 15:03:43 +0200

    Add solution for part 2 of 2022 Day 3 in C#.

--- 2022/Day 3/Program.cs
+++ 2022/Day 3/Program.cs
@@ -5,8 +5,14 @@ class Program
     {
         string path = "input.txt";
         List<string> input = GetListFromFile(path);
-        List<char> sharedItems = GetSharedItems(input);
-        int prioritySum = GetPrioritySumOfSharedItems(sharedItems);
+
+        // Part 1
+        // List<char> sharedItems = GetSharedItems(input);
+
+        // Part 2
+        List<char> sharedBadges = GetSharedBadges(input);
+
+        int prioritySum = GetPrioritySum(sharedBadges);
         Console.WriteLine("Total sum of priority is {0}", prioritySum);
     }
 
@@ -51,7 +57,7 @@ class Program
         return sharedItems;
     }
 
-    public static int GetPrioritySumOfSharedItems(List<char> sharedItems)
+    public static int GetPrioritySum(List<char> sharedItems)
     {
         int sum = 0;
         foreach (char item in sharedItems)
@@ -66,5 +72,34 @@ class Program
             }
         }
         return sum;
+    }
+
+    public static List<char> GetSharedBadges(List<string> input)
+    {
+        List<char> sharedBadges = new List<char>();
+        string[] elfGroup = new string[3];
+        int elfIndex = 0;
+        for (int i = 0; i < input.Count; i++)
+        {
+            elfGroup[elfIndex] = input[i];
+            elfIndex++;
+            if ((i + 1) % 3 == 0)
+            {
+                elfGroup = elfGroup.OrderByDescending(elf => elf.Length).ToArray();
+                string elfWithMaxItems = elfGroup[0];
+                for (int j = 0; j < elfWithMaxItems.Length; j++)
+                {
+                    char badge = elfWithMaxItems[j];
+                    if (elfGroup[1].Contains(badge) && elfGroup[2].Contains(badge))
+                    {
+                        sharedBadges.Add(badge);
+                        elfGroup = new string[3];
+                        elfIndex = 0;
+                        break;
+                    }
+                }
+            }
+        }
+        return sharedBadges;
     }
 }
